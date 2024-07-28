974ae02b6b05949c7f618ffbfbd1a5236cbaba15
Renato Blasco <renatoblascodev@gmail.com>
2024-07-27 23:52:54 -0300

    remove trash file

--- _init copy.sql
+++
@@ -1,8 +0,0 @@
--- Cria um novo usuário
-CREATE USER postgres WITH PASSWORD 'Yuri100100';
-
--- Cria um banco de dados
-CREATE DATABASE acesso;
-
--- Concede permissões ao usuário no banco de dados
-GRANT ALL PRIVILEGES ON DATABASE acesso TO postgres;
