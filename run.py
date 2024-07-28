d8aa16b4b548470ba3df96d9eabb639d8791a294
Renato Blasco <renatoblascodev@gmail.com>
2024-07-28 09:09:03 -0300

    Adjust ambient

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     run(
         "app.main:app",
         host="0.0.0.0",
-        port=8010,
+        port=8011,
         workers=1,
         use_colors=True,
         log_config="app/log_conf.yaml",
